c02513f25df86af30dc5fccb05463797a21ff3da
heruji <heruji@outlook.com>
2019-11-30 19:04:30 +0800

    Modify the permutation method for time window check. This submission made us top 1.

--- a_careful_approach.c
+++ a_careful_approach.c
@@ -19,54 +19,52 @@ typedef struct pair {
 
 // landing scenarios, each containing a start time and an end time.
 pair timetable[MAX_N];
+// pointers to items in timetable.
+pair *p_timetable[MAX_N];
 
-// used in depth-fisrt search to help record whether an item in timetable
-// is visited or not
-bool visited[MAX_N]; 
+// swap ith and jth items in `p_timetable`
+void swap(int i, int j) {
+    pair *t;
 
-// check whether all the `n` items in timetable are visited
-bool all_visited(int n) {
-    int i;
-
-    for (i = 0; i < n; i++) {
-        if (!visited[i]) {
-            return false;
-        }
+    if (i == j) {
+        return;
     }
 
-    return true;
+    t = p_timetable[i];
+    p_timetable[i] = p_timetable[j];
+    p_timetable[j] = t;
 }
 
-// depth fisrt search. check whether the given `window` is feasible or not
-bool check_time_window_dfs(int n, double window, double start_time) {
+
+// check whether the given `window` is feasible or not
+bool check_time_window(int n, double window, double start_time) {
     int i;
+    bool feasible;
     double mininal_next_start_time, next_start_time;
 
     // the `window` is feasible if all items in timetable are visited
-    if (all_visited(n)) {
+    if (n == 0) {
         return true;
     }
-
 
     // increase start time by `window` each time
     mininal_next_start_time = start_time + window;
 
     // traverse through the `timetable`. recursively check `window`
     for (i = 0; i < n; i++) {
-        
-        if (!visited[i] && mininal_next_start_time <= timetable[i].end) {
-            visited[i] = true;
-
+        if (mininal_next_start_time <= p_timetable[i]->end) {
             // decide `next_start_time` greedily
             next_start_time = (
-                mininal_next_start_time > timetable[i].start ?
-                mininal_next_start_time : timetable[i].start);
+                mininal_next_start_time > p_timetable[i]->start ?
+                mininal_next_start_time : p_timetable[i]->start);
 
-            if (check_time_window_dfs(n, window, next_start_time)) {
+            swap(i, n - 1);
+            feasible = check_time_window(n - 1, window, next_start_time);
+            swap(i, n - 1);
+
+            if (feasible) {
                 return true;
             }
-
-            visited[i] = false;
         }
     }
     
@@ -77,25 +75,34 @@ bool check_time_window_dfs(int n, double window, double start_time) {
 int main(void) {
     int i, j, n, case_index;
     int minute, second;
-    double window, window_low, window_high;
+    bool feasible;
+    double window, window_low, window_high, start_time;
     double min_start, max_end;
 
+    // used in output to indicate the case number
     case_index = 1;
+
     while (scanf("%d", &n) != EOF) {
         // exit if n is zero
         if (n == 0) {
             break;
         }
 
-        // initialize timetable
+        // initialize `timetable` and `p_timetable`
         // we will need the mininal start time and the maximal end time
         min_start = 1440;
         max_end = 0;
+
         for (i = 0; i < n; i++) {
             scanf("%lf%lf", &timetable[i].start, &timetable[i].end);
+            p_timetable[i] = timetable + i;
+
+            // update `min_start`
             if (timetable[i].start < min_start) {
                 min_start = timetable[i].start;
             }
+
+            // update `max_end`
             if (timetable[i].end > max_end) {
                 max_end = timetable[i].end;
             }
@@ -107,23 +114,29 @@ int main(void) {
         // upper bound can be decided by `max_end`, `min_start` and `n`
         window_low = 0;
         window_high = (max_end - min_start) / (n - 1);
+
         // adjust the `ITER_NUM` here to make sure we have good precision for
         // `second`, so that it is rounded to a correct integer
         for (i = 0; i < ITER_NUM; i++) {
             window = (window_low + window_high) / 2;
 
             for (j = 0; j < n; j++) {
-                memset(visited, 0, sizeof(visited));
-                visited[j] = true;
+                // note that `start_time` should be fetched out of two `swap`
+                start_time = p_timetable[j]->start;
+
+                swap(j, n - 1);
+                feasible = check_time_window(n - 1, window, start_time);
+                swap(j, n - 1);
 
                 // if feasible, increase lower bound
-                if (check_time_window_dfs(n, window, timetable[j].start)) {
+                if (feasible) {
                     window_low = window;
                     break;
                 }
             }
+
             // if not feasible, decrease upper bound
-            if (j == n) {
+            if (!feasible) {
                 window_high = window;
             }
         }
